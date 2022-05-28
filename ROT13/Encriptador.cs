84164feaffd8dc40ece08d9e8f7fdf8e60bdb9bc
JimJorge69 <90728500+JimJorge69@users.noreply.github.com>
2022-05-28 18:33:25 -0500

    Agregar archivos de proyecto.

---
+++ ROT13/Encriptador.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ROT13
+{
+    class Encriptador
+    {
+        private LinkedList<Token> Salida;
+        private int a = 1, b = 2, c = 3, d = 4, e = 5, f = 6, g = 7, h = 8, i = 9, j = 10, k = 11, l = 12, m = 13, n = 14, ñ = 15, o = 16, p = 17, q = 18, r = 19, s = 20, t = 21, u = 22, v = 23, w = 24, x = 25, y = 26, z = 27;
+
+        public LinkedList<Token> escanear(String entrada)
+        {
+            Char caracter;
+            //Se inicia un siclo para la verificacion de la operacion sea correcta
+            for (int si = 0; si < entrada.Length; si++)
+            {
+                caracter = entrada.ElementAt(si);
+
+                if (caracter=='a')
+                {
+
+                }
+
+            }
+                return Salida;
+        }
+    }
+}
---
+++ ROT13/Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ROT13
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            buttonIniciar.BackColor = Color.Green;//Se coloca en color rojo el boton iniciar
+        }
+
+        private void buttonIniciar_Click(object sender, EventArgs e)
+        {
+
+            if (textBox1.Text == "")//Valido que el campo de texto este vacio
+            {
+                MessageBox.Show("Operacion Mal", "¡UPS!", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+            else//si no es vacio se inicia el analizador
+            {
+
+                String entrada = textBox1.Text;//Decalro el texto ingresado en un string
+                Encriptador lex = new Encriptador();//Se manda a llamar el objeto
+                LinkedList<Token> ltokens = lex.escanear(entrada);//Se crea una lista con TOKEN y se ingresa en el objeto
+
+                buttonIniciar.Enabled = false;//Desactivo el boton iniciar
+                buttonIniciar.BackColor = Color.Yellow;//Se coloca en color rojo el boton iniciar
+                textBox1.Enabled = false;//Se desabilita el campo del texto para mejor experiencia de usuario
+
+            }
+
+
+        }
+    }
+}
