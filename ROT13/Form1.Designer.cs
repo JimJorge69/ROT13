3448170d820df44c6681f97d739ce0650195b873
JimJorge69 <90728500+JimJorge69@users.noreply.github.com>
2022-06-03 10:38:45 -0500

    Cambios

--- ROT13/Form1.Designer.cs
+++ ROT13/Form1.Designer.cs
@@ -32,13 +32,23 @@ namespace ROT13
             this.buttonIniciar = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
             this.textBox1 = new System.Windows.Forms.TextBox();
+            this.lbEncriptado = new System.Windows.Forms.Label();
+            this.lbDesencriptado = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.lbTXTDesencriptado = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lbvl = new System.Windows.Forms.Label();
+            this.lbRot13 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // buttonIniciar
             // 
             this.buttonIniciar.BackColor = System.Drawing.SystemColors.ActiveCaption;
             this.buttonIniciar.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.buttonIniciar.Location = new System.Drawing.Point(301, 217);
+            this.buttonIniciar.Location = new System.Drawing.Point(189, 175);
             this.buttonIniciar.Name = "buttonIniciar";
             this.buttonIniciar.Size = new System.Drawing.Size(146, 28);
             this.buttonIniciar.TabIndex = 0;
@@ -50,30 +60,131 @@ namespace ROT13
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(247, 67);
+            this.label1.Location = new System.Drawing.Point(228, 66);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(277, 24);
+            this.label1.Size = new System.Drawing.Size(75, 24);
             this.label1.TabIndex = 1;
-            this.label1.Text = "ENCRIPTADOR 3000 ROT13";
+            this.label1.Text = "ROT13";
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(209, 144);
+            this.textBox1.Location = new System.Drawing.Point(98, 132);
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(349, 20);
             this.textBox1.TabIndex = 2;
             this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // lbEncriptado
+            // 
+            this.lbEncriptado.AutoSize = true;
+            this.lbEncriptado.Location = new System.Drawing.Point(611, 167);
+            this.lbEncriptado.Name = "lbEncriptado";
+            this.lbEncriptado.Size = new System.Drawing.Size(10, 13);
+            this.lbEncriptado.TabIndex = 4;
+            this.lbEncriptado.Text = "-";
+            // 
+            // lbDesencriptado
+            // 
+            this.lbDesencriptado.AutoSize = true;
+            this.lbDesencriptado.Location = new System.Drawing.Point(611, 225);
+            this.lbDesencriptado.Name = "lbDesencriptado";
+            this.lbDesencriptado.Size = new System.Drawing.Size(10, 13);
+            this.lbDesencriptado.TabIndex = 5;
+            this.lbDesencriptado.Text = "-";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(553, 32);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(96, 13);
+            this.label5.TabIndex = 6;
+            this.label5.Text = "Encriptado ROT13";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(553, 139);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(58, 13);
+            this.label6.TabIndex = 7;
+            this.label6.Text = "Encriptado";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(553, 199);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(76, 13);
+            this.label7.TabIndex = 8;
+            this.label7.Text = "Desencriptado";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(553, 268);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(104, 13);
+            this.label8.TabIndex = 9;
+            this.label8.Text = "Texto desencriptado";
+            // 
+            // lbTXTDesencriptado
+            // 
+            this.lbTXTDesencriptado.AutoSize = true;
+            this.lbTXTDesencriptado.Location = new System.Drawing.Point(614, 299);
+            this.lbTXTDesencriptado.Name = "lbTXTDesencriptado";
+            this.lbTXTDesencriptado.Size = new System.Drawing.Size(10, 13);
+            this.lbTXTDesencriptado.TabIndex = 10;
+            this.lbTXTDesencriptado.Text = "-";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(553, 87);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(161, 13);
+            this.label2.TabIndex = 11;
+            this.label2.Text = "Valores asignados para encriptar";
+            // 
+            // lbvl
+            // 
+            this.lbvl.AutoSize = true;
+            this.lbvl.Location = new System.Drawing.Point(611, 112);
+            this.lbvl.Name = "lbvl";
+            this.lbvl.Size = new System.Drawing.Size(10, 13);
+            this.lbvl.TabIndex = 12;
+            this.lbvl.Text = "-";
+            // 
+            // lbRot13
+            // 
+            this.lbRot13.AutoSize = true;
+            this.lbRot13.Location = new System.Drawing.Point(614, 49);
+            this.lbRot13.Name = "lbRot13";
+            this.lbRot13.Size = new System.Drawing.Size(10, 13);
+            this.lbRot13.TabIndex = 13;
+            this.lbRot13.Text = "-";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lbRot13);
+            this.Controls.Add(this.lbvl);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.lbTXTDesencriptado);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.lbDesencriptado);
+            this.Controls.Add(this.lbEncriptado);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.buttonIniciar);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -84,6 +195,16 @@ namespace ROT13
         private System.Windows.Forms.Button buttonIniciar;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label lbEncriptado;
+        private System.Windows.Forms.Label lbDesencriptado;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label lbTXTDesencriptado;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lbvl;
+        private System.Windows.Forms.Label lbRot13;
     }
 }
 
