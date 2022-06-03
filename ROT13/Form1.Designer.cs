09d25da318f1bf80f0da11588e71929b4c312a2d
JimJorge69 <90728500+JimJorge69@users.noreply.github.com>
2022-06-03 10:54:49 -0500

    Cambios

--- ROT13/Form1.Designer.cs
+++ ROT13/Form1.Designer.cs
@@ -77,86 +77,106 @@ namespace ROT13
             // lbEncriptado
             // 
             this.lbEncriptado.AutoSize = true;
+            this.lbEncriptado.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.lbEncriptado.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(161)));
             this.lbEncriptado.Location = new System.Drawing.Point(61, 388);
             this.lbEncriptado.Name = "lbEncriptado";
-            this.lbEncriptado.Size = new System.Drawing.Size(0, 13);
+            this.lbEncriptado.Size = new System.Drawing.Size(0, 16);
             this.lbEncriptado.TabIndex = 4;
             // 
             // lbDesencriptado
             // 
             this.lbDesencriptado.AutoSize = true;
+            this.lbDesencriptado.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.lbDesencriptado.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(161)));
             this.lbDesencriptado.Location = new System.Drawing.Point(479, 278);
             this.lbDesencriptado.Name = "lbDesencriptado";
-            this.lbDesencriptado.Size = new System.Drawing.Size(0, 13);
+            this.lbDesencriptado.Size = new System.Drawing.Size(0, 16);
             this.lbDesencriptado.TabIndex = 5;
             // 
             // label5
             // 
             this.label5.AutoSize = true;
+            this.label5.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(161)));
             this.label5.Location = new System.Drawing.Point(3, 253);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(96, 13);
+            this.label5.Size = new System.Drawing.Size(135, 16);
             this.label5.TabIndex = 6;
             this.label5.Text = "Encriptado ROT13";
             // 
             // label6
             // 
             this.label6.AutoSize = true;
+            this.label6.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(161)));
             this.label6.Location = new System.Drawing.Point(3, 360);
             this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(58, 13);
+            this.label6.Size = new System.Drawing.Size(83, 16);
             this.label6.TabIndex = 7;
             this.label6.Text = "Encriptado";
             // 
             // label7
             // 
             this.label7.AutoSize = true;
+            this.label7.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(161)));
             this.label7.Location = new System.Drawing.Point(421, 252);
             this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(76, 13);
+            this.label7.Size = new System.Drawing.Size(110, 16);
             this.label7.TabIndex = 8;
             this.label7.Text = "Desencriptado";
             // 
             // label8
             // 
             this.label8.AutoSize = true;
+            this.label8.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(161)));
             this.label8.Location = new System.Drawing.Point(421, 321);
             this.label8.Name = "label8";
-            this.label8.Size = new System.Drawing.Size(104, 13);
+            this.label8.Size = new System.Drawing.Size(151, 16);
             this.label8.TabIndex = 9;
             this.label8.Text = "Texto desencriptado";
             // 
             // lbTXTDesencriptado
             // 
             this.lbTXTDesencriptado.AutoSize = true;
+            this.lbTXTDesencriptado.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.lbTXTDesencriptado.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(161)));
             this.lbTXTDesencriptado.Location = new System.Drawing.Point(482, 352);
             this.lbTXTDesencriptado.Name = "lbTXTDesencriptado";
-            this.lbTXTDesencriptado.Size = new System.Drawing.Size(0, 13);
+            this.lbTXTDesencriptado.Size = new System.Drawing.Size(0, 16);
             this.lbTXTDesencriptado.TabIndex = 10;
             // 
             // label2
             // 
             this.label2.AutoSize = true;
+            this.label2.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(161)));
             this.label2.Location = new System.Drawing.Point(3, 308);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(161, 13);
+            this.label2.Size = new System.Drawing.Size(240, 16);
             this.label2.TabIndex = 11;
             this.label2.Text = "Valores asignados para encriptar";
             // 
             // lbvl
             // 
             this.lbvl.AutoSize = true;
+            this.lbvl.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.lbvl.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(161)));
             this.lbvl.Location = new System.Drawing.Point(61, 333);
             this.lbvl.Name = "lbvl";
-            this.lbvl.Size = new System.Drawing.Size(0, 13);
+            this.lbvl.Size = new System.Drawing.Size(0, 16);
             this.lbvl.TabIndex = 12;
             // 
             // lbRot13
             // 
             this.lbRot13.AutoSize = true;
+            this.lbRot13.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.lbRot13.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(161)));
             this.lbRot13.Location = new System.Drawing.Point(64, 270);
             this.lbRot13.Name = "lbRot13";
-            this.lbRot13.Size = new System.Drawing.Size(0, 13);
+            this.lbRot13.Size = new System.Drawing.Size(0, 16);
             this.lbRot13.TabIndex = 13;
             // 
             // ROT13
