c790d21ef30e289790ffda9a7f3127424a9e378a
JimJorge69 <90728500+JimJorge69@users.noreply.github.com>
2022-06-03 10:56:12 -0500

    Cambios

--- ROT13/Form1.Designer.cs
+++ ROT13/Form1.Designer.cs
@@ -29,6 +29,7 @@ namespace ROT13
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ROT13));
             this.buttonIniciar = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
             this.textBox1 = new System.Windows.Forms.TextBox();
@@ -197,8 +198,9 @@ namespace ROT13
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.buttonIniciar);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "ROT13";
-            this.Text = "Form1";
+            this.Text = "ROT13";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
